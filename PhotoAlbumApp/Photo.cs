f1d3a637f0e954b3d75fda315d74eed23612f7db
Ahmed <khakunov@gmail.com>
2025-06-01 20:53:35 +0300

    Добавлены классы Photo и PhotoAlbum.

---
+++ PhotoAlbumApp/Photo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+public class Photo
+{
+    public string Path { get; set; }
+    public string Description { get; set; }
+    public DateTime DateTaken { get; set; }
+    public Photo(string path, string description, DateTime dateTaken)
+    {
+        Path = path;
+        Description = description;
+        DateTaken = dateTaken;
+    }
+    public override string ToString()
+    {
+        return $"{Path} - {Description} ({DateTaken.ToString("dd.MM.yyyy")})";
+    }
+}
---
+++ PhotoAlbumApp/PhotoAlbum.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+public class PhotoAlbum
+{
+    private List<Photo> photos = new List<Photo>();
+    private ListView listView;
+    public PhotoAlbum(ListView listView)
+    {
+        this.listView = listView;
+        LoadPhotos();
+    }
+    private void LoadPhotos()
+    {
+        listView.Items.Clear();
+        foreach (var photo in photos)
+        {
+            listView.Items.Add(new ListViewItem(new[] { photo.Path, photo.Description,
+photo.DateTaken.ToString("dd.MM.yyyy") }));
+        }
+    }
+    public void AddPhoto()
+    {
+        using (var openFileDialog = new OpenFileDialog())
+        {
+            openFileDialog.InitialDirectory =
+            Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            openFileDialog.Title = "Выберите фото";
+            openFileDialog.Filter = "Изображения (*.jpg;*.png;*.jpeg)|*.jpg;*.png;*.jpeg";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                var photoPath = openFileDialog.FileName;
+                var description = GetDescription();
+                var dateTaken = DateTime.ParseExact(description, "dd.MM.yyyy", null);
+                photos.Add(new Photo(photoPath, description, dateTaken));
+                LoadPhotos();
+                MessageBox.Show("Фото добавлено.");
+            }
+        }
+    }
+    public void RemovePhoto()
+    {
+        if (listView.SelectedItems.Count == 0)
+        {
+            MessageBox.Show("Сначала выберите фото для удаления.");
+            return;
+        }
+        var photoPath = listView.SelectedItems[0].SubItems[0].Text;
+        photos.RemoveAll(p => p.Path == photoPath);
+        LoadPhotos();
+        MessageBox.Show("Фото удалено.");
+    }
+    public void SortPhotosByDate()
+    {
+        var sortedPhotos = photos.OrderBy(p => p.DateTaken).ToList();
+        photos = new List<Photo>(sortedPhotos);
+        LoadPhotos();
+        MessageBox.Show("Фото отсортированы по дате.");
+    }
+    private string GetDescription()
+    {
+        return $"";
+    }
+}
