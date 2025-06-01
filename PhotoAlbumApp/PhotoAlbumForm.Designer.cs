feb66ab78657856a7a4c42623742a31db55f0472
Ahmed <khakunov@gmail.com>
2025-06-01 21:01:07 +0300

    Добавлен графический интерфейс.

--- PhotoAlbumApp/PhotoAlbumForm.Designer.cs
+++ PhotoAlbumApp/PhotoAlbumForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PhotoAlbumApp
 {
-    partial class Form1
+    partial class PhotoAlbumForm
     {
         /// <summary>
         /// Required designer variable.
@@ -31,7 +31,7 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "PhotoAlbumForm";
         }
 
         #endregion
---
+++ PhotoAlbumApp/PhotoAlbumForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhotoAlbumApp
+{
+    public partial class PhotoAlbumForm : Form
+    {
+        private PhotoAlbum photoAlbum;
+        private ListView listView;
+        private Button addPhotoButton;
+        private Button removePhotoButton;
+        private Button sortByDateButton;
+        public PhotoAlbumForm()
+        {
+            this.Text = "Управление фотографиями";
+            this.Width = 600;
+            this.Height = 400;
+            CreateControls();
+            photoAlbum = new PhotoAlbum(listView);
+        }
+        private void CreateControls()
+        {
+            listView = new ListView
+            {
+                Location = new System.Drawing.Point(10, 10),
+                Size = new System.Drawing.Size(580, 350),
+                View = View.Details,
+                FullRowSelect = true
+            };
+            listView.Columns.Add("Путь", 300);
+            listView.Columns.Add("Описание", 200);
+            listView.Columns.Add("Дата съёмки", 100);
+            addPhotoButton = new Button
+            {
+                Location = new System.Drawing.Point(10, 370),
+                Text = "Добавить фото",
+                Size = new System.Drawing.Size(100, 25)
+            };
+            addPhotoButton.Click += (sender, e) => photoAlbum.AddPhoto();
+            removePhotoButton = new Button
+            {
+                Location = new System.Drawing.Point(120, 370),
+                Text = "Удалить фото",
+                Size = new System.Drawing.Size(100, 25)
+            };
+            removePhotoButton.Click += (sender, e) => photoAlbum.RemovePhoto();
+            sortByDateButton = new Button
+            {
+                Location = new System.Drawing.Point(230, 370),
+                Text = "Отсортировать по дате",
+                Size = new System.Drawing.Size(120, 25)
+            };
+            sortByDateButton.Click += (sender, e) => photoAlbum.SortPhotosByDate();
+            this.Controls.Add(listView);
+            this.Controls.Add(addPhotoButton);
+            this.Controls.Add(removePhotoButton);
+            this.Controls.Add(sortByDateButton);
+        }
+    }
+}
